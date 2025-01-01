d7d145f4088668111bd093e44c16e97c8d222851
aziznal <aziznal.dev@gmail.com>
2025-01-01 23:47:32 +0300

    initial commit

---
+++ config.sh
@@ -0,0 +1,55 @@
+# Modeline 
+#	 vi: tabstop=4 filetype=sh
+
+# General 
+	# Show which segment fails and its exit code.
+	export TMUX_POWERLINE_DEBUG_MODE_ENABLED="false"
+	# Use patched font symbols.
+	export TMUX_POWERLINE_PATCHED_FONT_IN_USE="true"
+
+	# The theme to use.
+	export TMUX_POWERLINE_THEME="my-custom-theme"
+	# Overlay directory to look for themes. There you can put your own themes outside the repo. Fallback will still be the "themes" directory in the repo.
+	export TMUX_POWERLINE_DIR_USER_THEMES="$HOME/.config/tmux-powerline/themes"
+	# Overlay directory to look for segments. There you can put your own segments outside the repo. Fallback will still be the "segments" directory in the repo.
+	export TMUX_POWERLINE_DIR_USER_SEGMENTS="$HOME/.config/tmux-powerline/segments"
+
+	# The initial visibility of the status bar. Can be "on", "off", "2". 2 will create two status lines: one for the window list and one with status bar segments. 
+	export TMUX_POWERLINE_STATUS_VISIBILITY="on"
+	# In case of visibility = 2, where to display window status and where left/right status bars.
+	# 0: window status top, left/right status bottom; 1: window status bottom, left/right status top
+	export TMUX_POWERLINE_WINDOW_STATUS_LINE=0
+	# The status bar refresh interval in seconds.
+	# Note that events that force-refresh the status bar (such as window renaming) will ignore this.
+	export TMUX_POWERLINE_STATUS_INTERVAL="1"
+	# The location of the window list. Can be "absolute-centre, centre, left, right".
+	# Note that "absolute-centre" is only supported on `tmux -V` >= 3.2.
+	export TMUX_POWERLINE_STATUS_JUSTIFICATION="absolute-centre"
+
+	# The maximum length of the left status bar.
+	export TMUX_POWERLINE_STATUS_LEFT_LENGTH="60"
+	# The maximum length of the right status bar.
+	export TMUX_POWERLINE_STATUS_RIGHT_LENGTH="90"
+
+	# The separator to use between windows on the status bar.
+	export TMUX_POWERLINE_WINDOW_STATUS_SEPARATOR="  "
+
+# date.sh 
+	# date(1) format for the date. If you don't, for some reason, like ISO 8601 format you might want to have "%D" or "%m/%d/%Y".
+	export TMUX_POWERLINE_SEG_DATE_FORMAT="%F"
+
+# mode_indicator.sh 
+	# Whether the normal & prefix mode section should be enabled. Should be "true, "false".
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_NORMAL_AND_PREFIX_MODE_ENABLED="true"
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_NORMAL_MODE_TEXT="normal"
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_PREFIX_MODE_TEXT="prefix"
+
+	# Whether the mouse mode section should be enabled. Should be "true, "false".
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_MOUSE_MODE_ENABLED="false"
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_MOUSE_MODE_TEXT="mouse"
+
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_COPY_MODE_ENABLED="true"
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_COPY_MODE_TEXT="copy"
+
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_SEPARATOR_TEXT=" • "
+
---
+++ themes/my-custom-theme.sh
@@ -0,0 +1,33 @@
+# See Color formatting section below for details on what colors can be used here.
+TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=#282a36
+TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=#BD93F9
+
+# See `man tmux` for additional formatting options for the status line.
+# The `format regular` and `format inverse` functions are provided as conveniences
+
+if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_CURRENT" ]; then
+	TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
+		"#[$(format inverse)]"
+		" #I: #W "
+		"#[$(format regular)]"
+	)
+fi
+
+if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_STYLE" ]; then
+	TMUX_POWERLINE_WINDOW_STATUS_STYLE=(
+		"$(format regular)"
+	)
+fi
+
+if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_FORMAT" ]; then
+	TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
+		"#[$(format regular)]"
+		" #I: #W "
+	)
+fi
+
+if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
+	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+		"mode_indicator ${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR} #6272A4"
+	)
+fi
