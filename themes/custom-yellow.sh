e6b10ec13f3ab430e6c56c1c0335ae3ff99342c6
aziznal <aziznal.dev@gmail.com>
2025-06-29 09:55:07 +0300

    change theme to yellow

---
+++ themes/custom-yellow.sh
@@ -0,0 +1,36 @@
+# See Color formatting section below for details on what colors can be used here.
+TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=#967600
+TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=#ffbf00
+
+	# The separator to use between windows on the status bar.
+	export TMUX_POWERLINE_WINDOW_STATUS_SEPARATOR="  "
+
+# See `man tmux` for additional formatting options for the status line.
+# The `format regular` and `format inverse` functions are provided as conveniences
+
+if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_CURRENT" ]; then
+	TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
+		"#[$(format inverse)]"
+		" #I: #W "
+		"#[$(format regular)]"
+	)
+fi
+
+if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_STYLE" ]; then
+	TMUX_POWERLINE_WINDOW_STATUS_STYLE=(
+		"$(format regular)"
+	)
+fi
+
+if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_FORMAT" ]; then
+	TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
+		"#[$(format regular)]"
+		" #I: #W "
+	)
+fi
+
+if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
+	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+		"mode_indicator ${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR} #deda14"
+	)
+fi
